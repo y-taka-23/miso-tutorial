a7895fc3318d0208406c6815a0225a1fa8a45033
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-02-11 00:52:25 +0900

    Define (de)serialization

--- 06-fetching-resources/src/Model.hs
+++ 06-fetching-resources/src/Model.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Model where
+
+import           Data.Aeson
 
 data Model = Model
     { players :: Maybe [Player]
@@ -16,3 +19,16 @@ data Player = Player
     , name  :: String
     , level :: Int
     } deriving (Eq, Show)
+
+instance FromJSON Player where
+    parseJSON = withObject "Player" $ \v -> Player
+        <$> v .: "id"
+        <*> v .: "name"
+        <*> v .: "level"
+
+instance ToJSON Player where
+    toJSON (Player ident name level) = object
+        [ "id"    .= ident
+        , "name"  .= name
+        , "level" .= level
+        ]
