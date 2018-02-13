b7932319ece214d0ec27d9be3f8b6afc2c4eca01
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-02-14 03:13:54 +0900

    Fetch the player list from mocked DB

--- 06-fetching-resources/src/Effect.hs
+++ 06-fetching-resources/src/Effect.hs
@@ -2,10 +2,22 @@
 module Effect where
 
 import           Data.Aeson
+import           JavaScript.Web.XMLHttpRequest
 import           Model
 
 fetchPlayers :: IO (Either String [Player])
-fetchPlayers = pure []
+fetchPlayers = do
+    Just json <- contents <$> xhrByteString req
+    pure $ eitherDecodeStrict json
+    where
+        req = Request
+            { reqMethod = GET
+            , reqURI = "http://localhost:4000/players"
+            , reqLogin = Nothing
+            , reqHeaders = []
+            , reqWithCredentials = False
+            , reqData = NoData
+            }
 
 instance FromJSON Player where
     parseJSON = withObject "Player" $ \v -> Player
