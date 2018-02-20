08ce6b75e83a9f0894d2968f93ea365592859de5
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-02-20 14:48:11 +0900

    Add actions to handle the current URI

--- 07-routing/src/Update.hs
+++ 07-routing/src/Update.hs
@@ -10,4 +10,7 @@ updateModel :: Action -> Model -> Effect Action Model
 updateModel FetchPlayers m = m <# do
     SetPlayers <$> fetchPlayers
 updateModel (SetPlayers ePs) m = noEff m { players = ePs }
+updateModel (ChangeURI uri) m = m <# do
+    pushURI uri >> pure NoOp
+updateModel (HandleURI uri) m = noEff m { currentURI = uri }
 updateModel NoOp m = noEff m
