3d7e2ff31ba627fc0fc700d87e4579d0dfa1464e
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-02-22 02:49:13 +0900

    Implement routing

--- 07-routing/src/Model.hs
+++ 07-routing/src/Model.hs
@@ -1,11 +1,17 @@
 module Model where
 
-import           Network.URI
+import           Miso
 
 data Model = Model
     { players    :: Either String [Player]
     , currentURI :: URI
     } deriving (Eq, Show)
+
+instance HasURI Model where
+    lensURI = makeLens getter setter
+        where
+            getter m     = currentURI m
+            setter m uri = m { currentURI = uri }
 
 initialModel :: URI -> Model
 initialModel uri = Model
