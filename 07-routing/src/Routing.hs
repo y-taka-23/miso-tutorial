7d585b715e759d9cfc0707c3156a4cf4ed38391d
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-02-24 22:36:12 +0900

    Remove unused constants

--- 07-routing/src/Routing.hs
+++ 07-routing/src/Routing.hs
@@ -21,15 +21,6 @@ type ListRoute = "players" :> View Action
 
 type EditRoute = "players" :> Capture "ident" PlayerId :> View Action
 
-topRoute :: Proxy TopRoute
-topRoute = Proxy
-
-listRoute :: Proxy ListRoute
-listRoute = Proxy
-
-editRoute :: Proxy EditRoute
-editRoute = Proxy
-
 goList :: Action
 goList =
     ChangeURI $ safeLink (Proxy :: Proxy Route) (Proxy :: Proxy ListRoute)
